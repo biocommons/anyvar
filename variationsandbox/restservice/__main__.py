471852e3e4b543d6e260c0bbe1fefd5c711d2983
Reece Hart <reecehart@gmail.com>
2018-11-25 10:24:15 -0800

    renamed to variationsandbox

--- variationsandbox/restservice/__main__.py
+++ variationsandbox/restservice/__main__.py
@@ -7,7 +7,7 @@ fn = resource_filename(__name__, "openapi.yaml")
 
 app = connexion.App(__name__, debug=True,
                     specification_dir="openapi")
-app.add_api(fn, resolver=RestyResolver("variationregistry.restservice.restapi"))
+app.add_api(fn, resolver=RestyResolver("variationsandbox.restservice.restapi"))
 
 # e.g., http://0.0.0.0:9090/v0/ui/
 app.run(port=9090)
