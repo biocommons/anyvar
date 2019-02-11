86365ff966b1de7dc932b971ecb954f3198481f9
Reece Hart <reecehart@gmail.com>
2019-02-10 22:14:13 -0800

    require explicit create_app() in variationsandbox/app.py

--- src/variationsandbox/__main__.py
+++ src/variationsandbox/__main__.py
@@ -1,26 +1,21 @@
-import logging
-import sys
-
 import connexion
-from flask import redirect
 from connexion.resolver import RestyResolver
+from flask import redirect
 from pkg_resources import resource_filename
 
 
-_logger = logging.getLogger(__name__)
+def create_app(debug=False):
+    app = connexion.App(__name__, debug=debug)
+    fn = resource_filename(__name__, "/openapi.yaml")
+    app.add_api(fn, resolver=RestyResolver("variationsandbox"))
 
+    @app.route('/')
+    def index():
+        return redirect("/ui")
 
-app = connexion.App(__name__,
-                    debug=True,
-                    )
-fn = resource_filename(__name__, "/openapi.yaml")
-app.add_api(fn, resolver=RestyResolver("variationsandbox"))
-
-
-@app.route('/')
-def index():
-    return redirect("/ui")
+    return app
 
 
 if __name__ == "__main__":
+    app = create_app(debug=True)
     app.run(host="localhost")
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,11 @@
 import pytest
 import connexion
 
-from variationsandbox.app import app
+from variationsandbox.app import create_app
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="module")
 def client():
+    app = create_app()
     with app.app.test_client() as c:
         yield c
