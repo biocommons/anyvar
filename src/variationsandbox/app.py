08b98e1f827401cc4ff4d9d1312c84bd358cb3e3
Reece Hart <reecehart@gmail.com>
2019-02-15 16:07:06 -0800

    [mq]: variation-endpoint

--- src/variationsandbox/app.py
+++ src/variationsandbox/app.py
@@ -5,17 +5,17 @@ from pkg_resources import resource_filename
 
 
 def create_app(debug=False):
-    app = connexion.App(__name__, debug=debug)
+    cxapp = connexion.App(__name__, debug=debug)
     fn = resource_filename(__name__, "/openapi.yaml")
-    app.add_api(fn, resolver=RestyResolver("variationsandbox"))
+    cxapp.add_api(fn, resolver=RestyResolver("variationsandbox"))
 
-    @app.route('/')
+    @cxapp.route('/')
     def index():
         return redirect("/ui")
 
-    return app
+    return cxapp
 
 
 if __name__ == "__main__":
-    app = create_app(debug=True)
-    app.run(host="localhost")
+    cxapp = create_app(debug=True)
+    cxapp.run(host="localhost")
---
+++ src/variationsandbox/utils.py
@@ -0,0 +1,66 @@
+import re
+
+
+
+
+format_regexps = {
+    "hgvs": [
+        # just the accession and variant type
+        r"^[^:]+:[cgnopr]",
+    ],
+    "spdi": [
+        # SequenceId:Position:DeletionLength:InsertedSequence
+        r"^[^:]+:\d+:(\d+|\w*):\w*"
+    ],
+    "gnomad": [
+        # 1-55516888-G-GA
+        r"^\d+-\d+-\w*-\w*$",
+    ],
+    "beacon": [
+        
+    ],
+}
+
+format_regexps = {
+    t: [re.compile(e) for e in exprs]
+    for t, exprs in format_regexps.items()}
+
+
+
+def infer_plausible_formats(o):
+    """Returns a *set* of plausible formats of the given variation
+    definition.  Format inference is permissive: that is, all
+    well-formed variation of a particular syntax should be correctly
+    recognized, but some invalid variation may be incorrectly
+    recognized.  This function will typically return a set with 0 or 1
+    item.
+
+    Recognized string formats:
+    * "hgvs": NM_000551.3:c.456A>T
+    * "spdi": e.g., Seq1:4:AT:CCC
+    * "beacon": e.g., 13 : 32936732 G > C
+    * "gnomad": 1-55516888-G-GA
+
+    If the input is a list, then the resulting set is the
+    *intersection* of this function applied to all members of the
+    list.  A list of lists (i.e., a list of list of haplotypes that
+    forms a genotype) is supported. Because the intersection of
+    inferred types is returned, the data are expected to be
+    homogeneously typed. That is, this function is not intended to
+    handle cases of a haplotype defined by alleles in different
+    formats.
+
+    """
+
+    if o is None:
+        return []
+
+    if isinstance(o, list):
+        return(set.intersection(infer_plausible_formats(elem) for elem in o))
+    
+    if isinstance(o, str):
+        return set(t
+                   for t, exprs in format_regexps.items()
+                   if any(e.match(o) for e in exprs))
+
+    raise RuntimeError("Cannot infer format of a " + type(o))
