4d1299f11bf0b489943cde8dd230eb41252c598e
Reece Hart <reecehart@gmail.com>
2019-05-08 12:18:32 -0700

    don't update repo when docker-build'ing

--- src/anyvar/__init__.py
+++ src/anyvar/__init__.py
@@ -1,18 +1,10 @@
 # -*- coding: utf-8 -*-
-from __future__ import absolute_import, division, print_function, unicode_literals
 
-import logging
 import pkg_resources
-import re
-import warnings
-
-logger = logging.getLogger(__name__)
 
 try:
     __version__ = pkg_resources.get_distribution(__name__).version
-    if re.match(r"^\d+\.\d+\.\d+$", __version__) is not None:
-        _is_released_version = True
-except pkg_resources.DistributionNotFound as e:
-    warnings.warn("can't get __version__ because %s package isn't installed" % __package__, Warning)
-    __version__ = None
-
+except pkg_resources.DistributionNotFound:
+    __version__ = "unknown"
+finally:
+    del pkg_resources
