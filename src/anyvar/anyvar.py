e16b4ee9c62ec5dfa035b513f84c65627a512d83
Reece Hart <reecehart@gmail.com>
2020-09-15 17:11:08 -0700

    demonstrated round-tripping of simple allele, including enref/deref

--- src/anyvar/anyvar.py
+++ src/anyvar/anyvar.py
@@ -7,8 +7,7 @@ import collections.abc
 import logging
 
 from ga4gh.core import ga4gh_identify
-import ga4gh.vr
-
+from ga4gh.vr import models, vr_deref, vr_enref
 from ga4gh.vr.extras.translator import Translator
 
 
@@ -16,53 +15,27 @@ _logger = logging.getLogger(__name__)
 
 
 class AnyVar:
-    def __init__(self, /, data_proxy, storage):
-        if not isinstance(storage, collections.abc.MutableMapping):
-            _logger.warning("AnyVar(storage=) should be a mutable mapping; you're on your own")
+    def __init__(self, /, data_proxy, object_store):
+        if not isinstance(object_store, collections.abc.MutableMapping):
+            _logger.warning("AnyVar(object_store=) should be a mutable mapping; you're on your own")
 
         self.data_proxy = data_proxy
-        self.storage = storage
+        self.object_store = object_store
         self.translator = Translator(data_proxy=data_proxy)
 
-    # TODO: instead of storing in separate "silos" (see storage), just
-    # store in one.
-    # Also, use vr_enref to do this, which will recurse for us.
+    def put_object(self, vo):
+        v = vr_enref(vo, self.object_store)
+        return str(v._id)
 
-    def add_allele(self, allele):
-        allele._id = ga4gh_identify(allele)
-        self.storage.alleles[allele._id] = allele
-        self.add_location(allele.location)
-        _logger.info(f"Added Allele {allele._id}")
-        return allele
-
-    def add_location(self, location):
-        location._id = ga4gh_identify(location)
-        self.storage.locations[location._id] = location
-        return location
-
-    def add_text(self, text):
-        text._id = ga4gh_identify(text)
-        self.storage.texts[text._id] = text
-        return text
-
-
-    # TODO: replace getters with direct access
-    def get_allele(self, id):
-        return self.storage.alleles[id]
-
-    def get_location(self, id):
-        return self.storage.location[id]
-
-    def get_text(self, id):
-        return self.storage.texts[id]
-
+    def get_object(self, id, deref=False):
+        v = self.object_store[id]
+        return vr_deref(v, self.object_store) if deref else v
     
-
     def translate_allele(self, defn, fmt=None):
         t = self.translator
 
         if fmt == "ga4gh":
-            a = ga4gh.vr.models.Allele(**defn)
+            a = models.Allele(**defn)
         elif fmt == "beacon":
             a = t.from_beacon(defn)
         elif fmt == "hgvs":
@@ -75,12 +48,27 @@ class AnyVar:
             raise ValueError(f"unsupported format ({fmt})")
 
         return a
-    
 
     def translate_text(self, defn):
-        t = ga4gh.vr.models.Text(definition=defn)
+        t = models.Text(definition=defn)
         return t
 
 
 if __name__ == "__main__":
-    from ga4gh.vr.dataproxy import 
+    import os
+    from biocommons.seqrepo import SeqRepo
+    from ga4gh.vr.dataproxy import SeqRepoDataProxy
+
+    seqrepo_dir = os.environ.get("SEQREPO_DIR", "/usr/local/share/seqrepo/latest")
+    data_proxy = SeqRepoDataProxy(SeqRepo(root_dir=seqrepo_dir))
+    object_store = {}
+         
+    av = AnyVar(data_proxy=data_proxy, object_store=object_store)
+    
+    v = av.translate_allele("NM_000551.3:c.1A>T", fmt="hgvs")
+    vid = av.put_object(v)
+
+    v2 = av.get_object(vid, deref=True)
+    assert v == v2              # roundtrip test
+    
+    
