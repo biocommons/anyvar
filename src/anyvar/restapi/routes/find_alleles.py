d1c16d804c8ab96140265f67ae38163427396780
korikuzma <korikuzma@gmail.com>
2022-07-12 17:46:02 -0500

    separate out getting ga4gh alias

--- src/anyvar/restapi/routes/find_alleles.py
+++ src/anyvar/restapi/routes/find_alleles.py
@@ -3,22 +3,27 @@ from connexion import NoContent
 from ..globals import get_anyvar
 from anyvar.storage.postgres import PostgresObjectStore
 
+def get_ga4gh_alias(seqrepo_data_proxy, accession):
+    md = seqrepo_data_proxy.get_metadata(accession)
+    aliases = md.get("aliases")
+    ga4gh_id = None
+    if aliases:
+        ga4gh_id = ([a for a in aliases if a.startswith("ga4gh")] or [None])[0]
+    return ga4gh_id
+
 def search(accession, start, stop):
     av = get_anyvar()
     try:
-        md = av.data_proxy.get_metadata(accession)
+        ga4gh_id = get_ga4gh_alias(av.data_proxy, accession)
     except KeyError:
         return [], 404
 
     alleles = []
-    aliases = md.get("aliases")
-    if aliases:
-        ga4gh_id = ([a for a in aliases if a.startswith("ga4gh")] or [None])[0]
-        if ga4gh_id:
-            if isinstance(av.object_store, (PostgresObjectStore)):
-                alleles = av.object_store.find_alleles(ga4gh_id, start, stop)
-            else:
-                raise NotImplementedError()
+    if ga4gh_id:
+        if isinstance(av.object_store, (PostgresObjectStore)):
+            alleles = av.object_store.find_alleles(ga4gh_id, start, stop)
+        else:
+            raise NotImplementedError()
 
     inline_alleles = list()
     for allele in alleles:
