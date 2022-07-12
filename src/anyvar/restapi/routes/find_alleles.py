7a67372471374a4378112020c6b32dc5e3949e54
korikuzma <korikuzma@gmail.com>
2022-07-12 16:14:08 -0500

    feat #12: add endpoint for querying allles in one genomic region

---
+++ src/anyvar/restapi/routes/find_alleles.py
@@ -0,0 +1,23 @@
+from connexion import NoContent
+
+from ..globals import get_anyvar
+
+
+def search(accession, start, stop):
+    av = get_anyvar()
+    try:
+        md = av.data_proxy.get_metadata(accession)
+    except KeyError:
+        return [], 404
+
+    alleles = []
+    aliases = md.get("aliases")
+    if aliases:
+        ga4gh_id = ([a for a in aliases if a.startswith("ga4gh")] or [None])[0]
+        if ga4gh_id:
+            alleles = av.object_store.find_alleles(ga4gh_id, start, stop)
+
+    inline_alleles = list()
+    for allele in alleles:
+        inline_alleles.append(av.get_object(allele["_id"], deref=True).as_dict())
+    return inline_alleles, 200
