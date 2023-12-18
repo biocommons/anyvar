51ced19a9168ebcc9039b6c651b6ee0cdabe910e
Eugene Clark <ehclark@partners.org>
2023-12-18 16:06:37 -0500

    Added unit tests for Snowflake integration
    test_snowflake.py tests the async
    batch write behavior using a mock Snowflake
    connection
    
    test_storage_mapping.py tests the general
    MutableMapping API implemented by the
    Snowflake integration

---
+++ tests/storage/test_storage_mapping.py
@@ -0,0 +1,55 @@
+"""Tests the mutable mapping API of the storage backend"""
+from ga4gh.vrs import vrs_enref
+
+from anyvar.translate.vrs_python import VrsPythonTranslator
+
+# pause for 5 seconds because Snowflake storage is an async write and
+#   tests will sometimes fail on test_storage_mapping
+def test_waitforsync():
+    import time
+    time.sleep(5)
+
+# __getitem__
+def test_getitem(storage, alleles):
+    for allele_id, allele in alleles.items():
+        assert storage[allele_id] is not None
+
+# __contains__
+def test_contains(storage, alleles):
+    for allele_id, allele in alleles.items():
+        assert allele_id in storage
+
+# __len__
+def test_len(storage):
+    assert len(storage) > 0
+
+# __iter__
+def test_iter(storage, alleles):
+    obj_iter = iter(storage)
+    count = 0
+    while True:
+        try:
+            obj = next(obj_iter)
+            count += 1
+        except StopIteration:
+            break
+    assert count == 11
+
+# keys
+def test_keys(storage, alleles):
+    key_list = storage.keys()
+    for allele_id, allele in alleles.items():
+        assert allele_id in key_list
+        
+# __delitem__
+def test_delitem(storage, alleles):
+    for allele_id, allele in alleles.items():
+        del storage[allele_id]
+
+# __setitem__
+def test_setitem(storage, alleles):
+    for allele_id, allele in alleles.items():
+        variation = allele["params"]
+        definition = variation["definition"]
+        translated_variation = VrsPythonTranslator().translate_variation(definition)
+        vrs_enref(translated_variation, storage)
