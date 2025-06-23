7dac2c328a57766e236f96a61538eb7d7d5f6418
Jennifer Bowser <jennifer.bowser@nationwidechildrens.org>
2025-06-23 13:44:10 -0400

    move the liftover logic into its own function so we can use early returns for error message annotations on liftover failure

--- src/anyvar/restapi/main.py
+++ src/anyvar/restapi/main.py
@@ -13,7 +13,6 @@ from http import HTTPStatus
 from typing import Annotated
 
 import ga4gh.vrs
-from agct import Converter, Genome, Strand
 from fastapi import (
     BackgroundTasks,
     Body,
@@ -28,8 +27,6 @@ from fastapi import (
     status,
 )
 from fastapi.responses import FileResponse
-from ga4gh.vrs import models
-from ga4gh.vrs.enderef import vrs_enref
 from pydantic import StrictStr
 
 import anyvar
@@ -57,8 +54,7 @@ from anyvar.translate.translate import (
     TranslationError,
     TranslatorConnectionError,
 )
-from anyvar.utils.functions import ReferenceAssemblyResolutionError
-from anyvar.utils.types import VrsObject, VrsVariation, variation_class_map
+from anyvar.utils.types import VrsVariation, variation_class_map
 
 try:
     import aiofiles  # noqa: I001
@@ -301,150 +297,34 @@ async def add_genomic_liftover_annotation(
     response = await call_next(request)
 
     # Only add liftover annotation on registration
-    registration_endpoints = ["/variation", "/vrs_variation"]
+    registration_endpoints = [
+        "/variation",
+        "/vrs_variation",
+    ]  # TODO: add support for "/vcf" registration
     if request.url.path in registration_endpoints:
         annotator: AnyAnnotation | None = getattr(
             request.app.state, "anyannotation", None
         )
         if annotator:
-            annotation_id = "liftover"
             response_json, new_response = await util_funcs.parse_and_rebuild_response(
                 response
             )
-            vrs_id = response_json.get(
-                "object_id", ""
-            )  # TODO: Handle cases where no vrs_id is found
-            liftover_annotation = annotator.get_annotation(vrs_id, annotation_id)
+            vrs_id = response_json.get("object_id", "")
+            if not vrs_id:  # If there's no vrs_id, registration was unsuccessful
+                return new_response  # Return the new response object since we have exhausted the response body iterator
 
-            # If we've already lifted over this variant before, no need to do it again
+            # Check if we've already lifted over this variant before - no need to do it twice
+            annotation_id = "liftover"
+            liftover_annotation = annotator.get_annotation(vrs_id, annotation_id)
             if liftover_annotation:
                 return new_response  # Return the new response object since we have exhausted the response body iterator
 
-            # Get variant start position, end position, and refget accession - liftover is currently unsupported without these
-            annotation_value = ""
-
+            # Perform the liftover
             variation_object = response_json.get("object", {})
-            refget_accession = (
-                variation_object.get("location", {})
-                .get("sequenceReference", {})
-                .get("refgetAccession")
+            seqrepo_dataproxy = request.app.state.anyvar.translator.dp
+            annotation_value = util_funcs.get_liftover_annotation(
+                variation_object, seqrepo_dataproxy
             )
-            start_position = variation_object.get("location", {}).get("start")
-            end_position = variation_object.get("location", {}).get("end")
-            if not refget_accession or not start_position or not end_position:
-                annotation_value = "unable to complete liftover: liftover is unsupported for variants without refget accession, start position and end position"
-            else:
-                # Determine which assembly we're converting from/to
-                av: AnyVar = request.app.state.anyvar
-                seqrepo_dataproxy = av.translator.dp
-                prefixed_accession = f"ga4gh:{refget_accession}"
-
-                grch37 = "GRCh37"
-                grch38 = "GRCh38"
-                aliases = {
-                    grch37: list(
-                        seqrepo_dataproxy.translate_sequence_identifier(
-                            prefixed_accession, grch37
-                        )
-                    ),
-                    grch38: list(
-                        seqrepo_dataproxy.translate_sequence_identifier(
-                            prefixed_accession, grch38
-                        )
-                    ),
-                }
-
-                from_assembly = None
-                to_assembly = None
-                chromosome = None
-                try:
-                    from_assembly, to_assembly = util_funcs.get_from_and_to_assemblies(
-                        aliases
-                    )
-                except ReferenceAssemblyResolutionError:
-                    annotation_value = (
-                        "unable to complete liftover: could not find liftover aliases"
-                    )
-                else:
-                    if not from_assembly and not to_assembly:
-                        annotation_value = "No liftover required: variant is identical in both assemblies"
-                    elif to_assembly and from_assembly:
-                        # Determine which chromosome we're on
-                        chromosome = util_funcs.get_chromosome_from_aliases(
-                            aliases.get(from_assembly, [])
-                        )
-
-                # Perform liftover conversion
-                if to_assembly and from_assembly and chromosome:
-                    assembly_map = {grch37: Genome.HG19, grch38: Genome.HG38}
-                    converter = Converter(
-                        assembly_map[from_assembly], assembly_map[to_assembly]
-                    )
-
-                    # Get converted start/end positions
-                    start_position = (
-                        util_funcs.convert_range_to_int(start_position, "lower")
-                        if isinstance(start_position, models.Range)
-                        else int(start_position)
-                    )
-                    end_position = (
-                        util_funcs.convert_range_to_int(end_position, "upper")
-                        if isinstance(end_position, models.Range)
-                        else int(end_position)
-                    )
-
-                    if start_position is None or end_position is None:
-                        annotation_value = "unable to complete liftover: could not convert start and/or end position to integer"
-                    else:
-                        converted_start = converter.convert_coordinate(
-                            chromosome,
-                            start_position,
-                            Strand.POSITIVE,
-                        )[0][1]
-                        converted_end = converter.convert_coordinate(
-                            chromosome,
-                            end_position,
-                            Strand.POSITIVE,
-                        )[0][1]
-
-                        # Get converted refget_accession (without 'ga4gh:' prefix)
-                        new_alias = f"{to_assembly}:{chromosome}"
-                        converted_refget_accession = (
-                            seqrepo_dataproxy.translate_sequence_identifier(
-                                new_alias, "ga4gh"
-                            )[0].split("ga4gh:")[1]
-                        )
-
-                        # Build the converted location dict
-                        converted_variation_location = {
-                            "start": converted_start,
-                            "end": converted_end,
-                            "id": None,
-                            "sequenceReference": {
-                                "type": "SequenceReference",
-                                "refgetAccession": converted_refget_accession,
-                            },
-                        }
-
-                        # Build the liftover variation object w/ converted location
-                        converted_variation_dict = variation_object
-                        converted_variation_dict["location"] = (
-                            converted_variation_location
-                        )
-                        # Get rid of the identifiers since these were from the original variation object and we need to re-compute them
-                        converted_variation_dict["digest"] = None
-                        converted_variation_dict["id"] = None
-
-                        # Convert the dict into a VrsObject class instance so we can compute the identifiers
-                        converted_variation_object: VrsObject = variation_class_map[
-                            variation_object.get("type")
-                        ](**converted_variation_dict)
-                        converted_variation_object = vrs_enref(
-                            converted_variation_object
-                        )
-
-                        # Convert the VrsObject back into a dict for storage
-                        annotation_value = converted_variation_object.model_dump()
 
             annotator.put_annotation(
                 object_id=vrs_id,
--- src/anyvar/utils/functions.py
+++ src/anyvar/utils/functions.py
@@ -2,15 +2,23 @@
 
 import json
 import re
-from typing import Literal
 
+from agct import Converter, Genome, Strand
 from fastapi import Response
 from fastapi.responses import JSONResponse
 from ga4gh.vrs import models
+from ga4gh.vrs.dataproxy import _DataProxy
+from ga4gh.vrs.enderef import vrs_enref
+
+from anyvar.utils.types import VrsObject, variation_class_map
 
 
-class ReferenceAssemblyResolutionError(Exception):
-    """Indicates a failure to resolve a variant's reference assembly"""
+class AliasRetrievalError(Exception):
+    """Indicates a failure to retrieve alias data for a refget accession in any supported reference assembly."""
+
+
+class AmbiguousReferenceAssemblyError(Exception):
+    """Indicates a failure to determine which reference assembly a variant is one due to alias matches in multiple reference assemblies, making the result ambiguous"""
 
 
 async def parse_and_rebuild_response(response: Response) -> tuple[dict, Response]:
@@ -59,7 +67,7 @@ def get_chromosome_from_aliases(aliases: list[str]) -> str | None:
     return f"chr{chromosome_number}"
 
 
-def get_from_and_to_assemblies(aliases: dict) -> tuple[str | None, str | None]:
+def get_from_and_to_assemblies(aliases: dict) -> tuple[str, str]:
     """Determine which assembly we're starting from, and which we're lifting over to.
 
     Takes in a dictionary containing two lists of aliases, one for each supported reference assembly (GRCh37 and GRCh38):
@@ -73,8 +81,6 @@ def get_from_and_to_assemblies(aliases: dict) -> tuple[str | None, str | None]:
     """
     grch37, grch38 = sorted(aliases.keys())
 
-    from_assembly = None
-    to_assembly = None
     if aliases[grch37] and not aliases[grch38]:
         from_assembly = grch37
         to_assembly = grch38
@@ -82,24 +88,138 @@ def get_from_and_to_assemblies(aliases: dict) -> tuple[str | None, str | None]:
         from_assembly = grch38
         to_assembly = grch37
     elif not aliases[grch37] and not aliases[grch38]:
-        raise ReferenceAssemblyResolutionError
+        raise AliasRetrievalError
+    else:
+        raise AmbiguousReferenceAssemblyError
 
     return from_assembly, to_assembly
 
 
-def convert_range_to_int(
-    range_object: models.Range, bound: Literal["lower", "upper"]
-) -> int | None:
-    """Convert a VRS Range object to an integer. If both an upper and lower bound are present, select for either the min or
-    max of the Range as dictated by the value of the param `bound`. If one bound is missing, returns the one that is
-    present. Returns `None` if both are missing.
+def convert_position(
+    converter: Converter, chromosome: str, position: models.Range | int
+) -> models.Range | int:
+    """Convert a SequenceLocation position (i.e., `start` or `end`) to another reference Genome. `position` can either be a `Range` or an `int` - return type will match.
 
-    ::param range_object: the VRS Range object to convert
-    :return: An `int` representing the specified upper or lower value in the Range if possible, else `None` if neither bound is defined
+    :param converter: An AGCT Converter instance.
+    :param chromosome: The chromosome number where the position is found. Must be a string consisting of the prefix "chr" plus a number, i.e. "chr10".
+    :param position: A SequenceLocation start or end position. Can be a `Range` or an `int`.
+
+    :return: A lifted-over position. Type (`Range` or `int`) will match that of `position`
     """
-    lower, upper = range_object.root
+    if isinstance(position, int):
+        return converter.convert_coordinate(chromosome, position, Strand.POSITIVE)[0][1]
 
-    if bound == "lower":
-        return lower or upper
+    lower, upper = position.root
+    lower = (
+        converter.convert_coordinate(chromosome, lower, Strand.POSITIVE)[0][1]
+        if lower
+        else None
+    )
+    upper = (
+        converter.convert_coordinate(chromosome, upper, Strand.POSITIVE)[0][1]
+        if upper
+        else None
+    )
+    return models.Range([lower, upper])
 
-    return upper or lower  # else: bound is 'upper'
+
+def get_liftover_annotation(
+    variation_object: dict, seqrepo_dataproxy: _DataProxy
+) -> str:
+    """Liftover a variant from GRCh37 or GRCH38 into the opposite assembly, and return the string identifier for the converted variant to be stored as an annotation.
+    If liftover is unsuccessful, return a string error message to store as an annotation instead.
+
+    :param variation_object: A dictionary representation of a VRS Object.
+    :param seqrepo_dataproxy: A SeqrepoDataproxy instance.
+    :return: The string ga4gh identifier of the lifted-over variant on success; else a string error message indicating why liftover was unsuccessful on failure.
+    """
+    error_msg = "Unable to complete liftover"
+
+    if not variation_object:
+        return f"{error_msg}: no variation found"
+
+    # Get variant start position, end position, and refget accession - liftover is currently unsupported without these
+    refget_accession = (
+        variation_object.get("location", {})
+        .get("sequenceReference", {})
+        .get("refgetAccession")
+    )
+    start_position = variation_object.get("location", {}).get("start")
+    end_position = variation_object.get("location", {}).get("end")
+    if not refget_accession or not start_position or not end_position:
+        return f"{error_msg}: liftover is unsupported for variants without refget accession, start position and end position"
+
+    # Determine which assembly we're converting from/to
+    prefixed_accession = f"ga4gh:{refget_accession}"
+
+    grch37 = "GRCh37"
+    grch38 = "GRCh38"
+    aliases = {
+        grch37: list(
+            seqrepo_dataproxy.translate_sequence_identifier(prefixed_accession, grch37)
+        ),
+        grch38: list(
+            seqrepo_dataproxy.translate_sequence_identifier(prefixed_accession, grch38)
+        ),
+    }
+
+    from_assembly = None
+    to_assembly = None
+    try:
+        from_assembly, to_assembly = get_from_and_to_assemblies(aliases)
+    except AliasRetrievalError:
+        return f"{error_msg}: could not resolve reference assembly - no alias data found in any supported assembly"
+    except AmbiguousReferenceAssemblyError:
+        return f"{error_msg}: could not resolve reference assembly - alias data found in multiple supported assemblies"
+
+    # Determine which chromosome the variant is on
+    chromosome = get_chromosome_from_aliases(aliases.get(from_assembly, []))
+    if not chromosome:
+        return f"{error_msg}: unable to resolve variant's chromosome"
+
+    # Begin liftover conversion
+    assembly_map = {grch37: Genome.HG19, grch38: Genome.HG38}
+    converter = Converter(assembly_map[from_assembly], assembly_map[to_assembly])
+
+    # Get converted start/end positions
+    converted_start = None
+    converted_end = None
+    try:
+        converted_start = convert_position(converter, chromosome, start_position)
+        converted_end = convert_position(converter, chromosome, end_position)
+    except Exception:
+        return f"{error_msg}: could not convert start and/or end position(s)"
+
+    # Get converted refget_accession (without 'ga4gh:' prefix)
+    new_alias = f"{to_assembly}:{chromosome}"
+    converted_refget_accession = seqrepo_dataproxy.translate_sequence_identifier(
+        new_alias, "ga4gh"
+    )[0].split("ga4gh:")[1]
+    if not converted_refget_accession:
+        return f"{error_msg}: could not convert refget accession"
+
+    # Build the converted location dict
+    converted_variation_location = {
+        "start": converted_start,
+        "end": converted_end,
+        "id": None,
+        "sequenceReference": {
+            "type": "SequenceReference",
+            "refgetAccession": converted_refget_accession,
+        },
+    }
+
+    # Build the liftover variation object w/ converted location
+    converted_variation_dict = variation_object
+    converted_variation_dict["location"] = converted_variation_location
+    # Get rid of the identifiers since these were from the original variation object and we need to re-compute them
+    converted_variation_dict["digest"] = None
+    converted_variation_dict["id"] = None
+
+    # Convert the dict into a VrsObject class instance so we can compute the identifiers
+    converted_variation_object: VrsObject = variation_class_map[
+        variation_object.get("type", "")
+    ](**converted_variation_dict)
+
+    # Compute the identifiers and return the ga4gh id of the converted variant
+    return vrs_enref(converted_variation_object, True)[1]
