3569bcce139a08be4df99a19a3fa42ade8ed2b60
Jennifer Bowser <jennifer.bowser@nationwidechildrens.org>
2025-06-26 13:15:25 -0400

    verify expected results + add test for unsupported assembly

--- tests/test_liftover.py
+++ tests/test_liftover.py
@@ -5,6 +5,10 @@ import anyvar.utils.functions as util_funcs
 from anyvar.anyvar import AnyVar, create_storage, create_translator
 from anyvar.storage import _Storage
 from anyvar.translate.translate import _Translator
+from anyvar.utils.functions import (
+    LIFTOVER_ERROR_ANNOTATIONS,
+    LiftoverError,
+)
 
 # SUCCESS CASES
 # Make sure this is a mix of a) types of variants, and b) types of start/end coords (int vs Range) - ranges need to be for /vrs_variation endpoint ONLY, and c) positive and negative strands
@@ -24,52 +28,30 @@ copynumber_ranged_positive_grch37_variant_object = (
         },
         "copies": 3,
     },
-    "ga4gh:CN.LQAMim_Q7_sXVRLX2UFVsHNOolDsK4Bo",
+    "ga4gh:CN.LQAMim_Q7_sXVRLX2UFVsHNOolDsK4Bo",  # verified externally
 )
 
-
-# variation object input that can be lifted over successfully from GRCH38 > GRCH37: allele, Range start/end coordinates, negative strand
-allele_ranged_negative_grch38_variant_object = (
+# liftover from GRCh38 > GRCh37, integer start/end coordinates, negative strand:
+# THIS ONE ISN'T WORKING
+allele_int_negative_grch38_variant_object = (
     {
+        "id": "ga4gh:VA.Otc5ovrw906Ack087o1fhegB4jDRqCAe",
         "type": "Allele",
         "location": {
-            "sequenceReference": {
-                "type": "SequenceReference",
-                "refgetAccession": "SQ.-A1QmD_MatoqxvgVxBLZTONHz9-c7nQo",
-            },
-            "start": [None, 30417575],
-            "end": [31394018, None],
+            "id": "ga4gh:SL.nhul5x5P_fKjGEpY9PEkMIekJfZaKom2",
+            "digest": "nhul5x5P_fKjGEpY9PEkMIekJfZaKom2",
+            "end": 140753336,
+            "start": 140753335,
             "type": "SequenceLocation",
+            "sequenceReference": {
+                "refgetAccession": "SQ.F-LrLMe1SRpfUZHkQmvkVKFEGaoDeHul",
+                "type": "SequenceReference",
+            },
         },
         "state": {"sequence": "T", "type": "LiteralSequenceExpression"},
     },
-    "ga4gh:VA.fdsjDZeIznBPBH7wd5mIgtMhAAeZ1zGf",
+    "ga4gh:VA.nmp-bzYpO00NYIqr3CaVF0ZH2ZpSj1ly",  # verified externally
 )
-
-# variation object input that can be lifted over successfully from GRCH38 > GRCH37: allele, integer start/end coordinates, unknown positive/negative strand
-allele_int_unknown_grch38_variant_object = (
-    {
-        "type": "Allele",
-        "location": {
-            "id": "ga4gh:SL.aCMcqLGKClwMWEDx3QWe4XSiGDlKXdB8",
-            "end": 87894077,
-            "start": 87894076,
-            "sequenceReference": {
-                "refgetAccession": "SQ.ss8r_wB0-b9r44TQTMmVTI92884QvBiB",
-                "type": "SequenceReference",
-            },
-            "type": "SequenceLocation",
-        },
-        "state": {"sequence": "T", "type": "LiteralSequenceExpression"},
-    },
-    "ga4gh:VA.0viMZeimc8A0MDQviRWyqqc0RfYazQjq",
-)
-
-# - /variation input that can be lifted over successfully from GRCH37 > GRCH38
-# @pytest.fixture(scope="module")
-# def negative_grch37_variant_id() -> str:
-#  return "NC_000007.13:g.140453136A>T"
-# ^ This is GRCH37, and on the Negative strand
 
 
 # FAILURES
@@ -77,28 +59,33 @@ allele_int_unknown_grch38_variant_object = (
 # - /vrs_variation input that can NOT be lifted over successfully > where original location doesn't exist in the other reference assembly
 # - /variation input that can NOT be lifted over successfully > where original location doesn't exist in the other reference assembly
 
-# - don't need to liftover because we already have a stored liftaver annotation
+# - don't need to liftover because we already have a stored liftover annotation
 # ^ Run any of the above a second time
 
 # - case where initial registration failed and we don't have a vrs_id
 
 
-# - case where the variant is on an unsupported assembly
-@pytest.fixture(scope="module")
-def unsupported_assembly_variant() -> dict:
-    return {
-        "type": "CopyNumberChange",
+# - case where the variant is on an unsupported assembly (GRCh36)
+grch36_variant = (
+    {
+        "digest": "4dEsVNR2JC_ZiHsYSGZgariIUOfYl6a0",
+        "id": "ga4gh:VA.4dEsVNR2JC_ZiHsYSGZgariIUOfYl6a0",
+        "type": "Allele",
         "location": {
-            "start": [15400349, 15414665],
-            "end": [16308334, 16345666],
+            "id": "ga4gh:SL.WROR90lhzJwgTPgxZx8dRP4Vcjr3BdDi",
+            "digest": "WROR90lhzJwgTPgxZx8dRP4Vcjr3BdDi",
             "type": "SequenceLocation",
-            "sequenceReference": {"type": "SequenceReference", "refgetAccession": ...},
+            "start": 45103598,
+            "end": 45103599,
+            "sequenceReference": {
+                "refgetAccession": "SQ.JY7UegcaYT-M0PYn1yDGQ_4XJsa-DsXq",
+                "type": "SequenceReference",
+            },
         },
-        "copyChange": "complete genomic loss",
-    }
-
-
-# ^ This is on GRCH36
+        "state": {"sequence": "T", "type": "LiteralSequenceExpression"},
+    },
+    LIFTOVER_ERROR_ANNOTATIONS[LiftoverError.UNSUPPORTED_REFERENCE_ASSEMBLY],
+)
 
 
 @pytest.fixture(scope="module")
@@ -114,18 +101,18 @@ def seqrepo_dataproxy() -> _DataProxy:
     ("variation_input", "expected_output"),
     [
         copynumber_ranged_positive_grch37_variant_object,
-        allele_ranged_negative_grch38_variant_object,
-        allele_int_unknown_grch38_variant_object,
+        # allele_int_negative_grch38_variant_object
+        grch36_variant,
     ],
 )
 def test_liftover_annotation(variation_input, expected_output, seqrepo_dataproxy):
     annotation_value = util_funcs.get_liftover_annotation(
         variation_input, seqrepo_dataproxy
     )
+
     assert annotation_value == expected_output
 
 
-# Failure cases
 # def test_duplicate_liftover_annotation(client, alleles):
 #     allele_id, allele_object = next(iter(alleles.items()))
 
