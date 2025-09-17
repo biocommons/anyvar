fdca3cedf6dc08806ead17984cef31e592e878b0
Kyle Ferriter <kferrite@broadinstitute.org>
2025-09-16 21:29:42 -0400

    Add abc.py

---
+++ src/anyvar/storage/abc.py
@@ -0,0 +1,67 @@
+"""Provide PostgreSQL-based storage implementation."""
+
+import enum
+from abc import ABC, abstractmethod
+from collections.abc import Iterable
+
+from ga4gh.vrs import models
+
+
+class StoredObjectType(enum.Enum):
+    """Supported VRS object types for AnyVar storage."""
+
+    ALLELE = "Allele"
+    COPY_NUMBER_COUNT = "CopyNumberCount"
+    COPY_NUMBER_CHANGE = "CopyNumberChange"
+    SEQUENCE_LOCATION = "SequenceLocation"
+    SEQUENCE_REFERENCE = "SequenceReference"
+
+
+class Storage(ABC):
+    """Abstract base class for interacting with storage backends."""
+
+    @abstractmethod
+    def setup(self) -> None:
+        """Set up the storage backend."""
+
+    @abstractmethod
+    def close(self) -> None:
+        """Close the storage backend."""
+
+    @abstractmethod
+    def add_objects(self, objects: Iterable[models.VrsType]) -> None:
+        """Add multiple VRS objects to storage."""
+
+    @abstractmethod
+    def get_objects(self, object_ids: Iterable[str]) -> Iterable[models.VrsType]:
+        """Retrieve multiple VRS objects from storage by their IDs."""
+
+    @abstractmethod
+    def get_all_object_ids(self) -> Iterable[str]:
+        """Retrieve all object IDs from storage."""
+
+    @abstractmethod
+    def get_object_count(self, object_type: StoredObjectType) -> int:
+        """Get count of objects of a specific type in storage."""
+
+    @abstractmethod
+    def delete_objects(
+        self, object_type: StoredObjectType, object_ids: Iterable[str]
+    ) -> None:
+        """Delete all objects of a specific type from storage."""
+
+    @abstractmethod
+    def search_alleles(
+        self,
+        refget_accession: str,
+        start: int,
+        stop: int,
+    ) -> list[models.Allele]:
+        """Find all Alleles in the particular region
+
+        :param refget_accession: refget accession (SQ. identifier)
+        :param start: Start genomic region to query
+        :param stop: Stop genomic region to query
+
+        :return: a list of Alleles
+        """
