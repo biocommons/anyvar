766d7c572320cb0a47091486be7f0f14112b08d7
Kori Kuzma <korikuzma@gmail.com>
2025-11-25 09:36:28 -0500

    chore: remove commented out tests
    close #307
    
    * created separate issues to add back (#331, #332)

--- tests/integration/crud/test_variation.py
+++ tests/integration/crud/test_variation.py
@@ -46,39 +46,3 @@ def test_get_allele(restapi_client: TestClient, preloaded_alleles):
         "/variation/ga4gh:VA.invalid7DSM9KE3Z0LntAukLqm0K2ENn"
     )
     assert bad_resp.status_code == HTTPStatus.NOT_FOUND
-
-
-# @pytest.fixture(scope="module")
-# def copy_numbers(test_data_dir) -> dict:
-#     """Provide copy_numbers fixture object."""
-#     with (test_data_dir / "variations.json").open() as f:
-#         return json.load(f)["copy_numbers"]
-
-# def test_get_copy_numbers(restapi_client, copy_numbers):
-#     for copy_number_id, copy_number in copy_numbers.items():
-#         resp = restapi_client.get(f"/variation/{copy_number_id}")
-#         assert resp.status_code == HTTPStatus.OK
-#         assert resp.json()["data"] == copy_number["copy_number_response"]["object"]
-
-#     bad_resp = restapi_client.get("/allele/ga4gh:CX.invalid")
-#     assert bad_resp.status_code == HTTPStatus.NOT_FOUND
-
-# def test_put_copy_number(restapi_client, copy_numbers):
-#     for copy_number_id, copy_number in copy_numbers.items():
-#         resp = restapi_client.put("/variation", json=copy_number["params"])
-#         assert resp.status_code == HTTPStatus.OK
-#         assert resp.json()["object"]["id"] == copy_number_id
-
-#     # try unsupported variation type
-#     resp = restapi_client.put("/variation", json={"definition": "BRAF amplification"})
-#     assert resp.status_code == HTTPStatus.OK
-#     resp_json = resp.json()
-#     assert resp_json["messages"] == ['Unable to translate "BRAF amplification"']
-#     assert "object" not in resp_json
-
-# def test_put_vrs_copy_number(restapi_client, copy_numbers):
-#     for copy_number_id, copy_number in copy_numbers.items():
-#         params = deepcopy(copy_number["copy_number_response"]["object"])
-#         resp = restapi_client.put("/vrs_variation", json=params)
-#         assert resp.status_code == HTTPStatus.OK
-#         assert resp.json()["object_id"] == copy_number_id
