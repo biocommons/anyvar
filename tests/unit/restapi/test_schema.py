45cb3f0bc8bbfe61cc53b4c547fdbd2536af222e
James Stevenson <james.stevenson@nationwidechildrens.org>
2026-01-27 20:08:32 -0500

    fix: use correct object vs variation

--- tests/unit/restapi/test_schema.py
+++ tests/unit/restapi/test_schema.py
@@ -2,13 +2,13 @@ from typing import get_args
 
 import pytest
 
-from anyvar.restapi.schema import SupportedObjectType
+from anyvar.restapi.schema import SupportedVariationType
 from anyvar.utils.funcs import camel_to_snake
-from anyvar.utils.types import VrsObject
+from anyvar.utils.types import VrsVariation
 
 
 @pytest.mark.ci_ok
 def test_supported_object_type():
-    for cls in get_args(VrsObject):
-        assert SupportedObjectType[camel_to_snake(cls.__name__)] == cls.__name__
-    assert len(get_args(VrsObject)) == len(SupportedObjectType)
+    for cls in get_args(VrsVariation):
+        assert SupportedVariationType[camel_to_snake(cls.__name__)] == cls.__name__
+    assert len(get_args(VrsVariation)) == len(SupportedVariationType)
