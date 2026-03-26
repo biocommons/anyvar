12d3126cf519b8a276dbb6e9aa4ffdff41a7a1a6
James Stevenson <james.stevenson@nationwidechildrens.org>
2026-03-26 10:52:54 -0400

    update test

--- tests/integration/restapi/test_search_router.py
+++ tests/integration/restapi/test_search_router.py
@@ -1,8 +1,15 @@
 """Test search functionality."""
 
+from collections.abc import AsyncGenerator
 from http import HTTPStatus
 
+import pytest_asyncio
+from cool_seq_tool.sources import UtaDatabase
 from fastapi.testclient import TestClient
+
+from anyvar.anyvar import AnyVar
+from anyvar.restapi.main import app as anyvar_restapi
+from anyvar.restapi.schema import ServiceInfo
 
 
 def test_search(restapi_client: TestClient, preloaded_alleles: dict):
@@ -26,6 +33,35 @@ def test_search(restapi_client: TestClient, preloaded_alleles: dict):
         assert resp_json["variations"][0] == allele["variation"]
 
 
-def test_gene_search(restapi_client: TestClient, preloaded_alleles: dict):
-    # TODO add some stuff in after figuring out ref build problem
-    pass
+@pytest_asyncio.fixture(scope="module")
+async def uta_enabled_restapi_client(
+    anyvar_instance: AnyVar,
+) -> AsyncGenerator[TestClient, None]:
+    """RestAPI client instance with UTA injected into app state.
+
+    Broken out from other test client instance because UTA access would require
+    additional mocks or a real instance to connect to
+    """
+    anyvar_restapi.state.anyvar = anyvar_instance
+    anyvar_restapi.state.service_info = ServiceInfo()
+
+    uta = await UtaDatabase.create()
+    await uta.create_pool()
+    anyvar_restapi.state.uta = uta
+    with TestClient(app=anyvar_restapi) as client:
+        yield client
+
+    await uta._connection_pool.close()  # noqa: SLF001
+
+
+def test_gene_search(uta_enabled_restapi_client: TestClient, alleles: dict):
+    # ensure DB populated
+    allele_to_put = alleles["ga4gh:VA.Otc5ovrw906Ack087o1fhegB4jDRqCAe"]["variation"]
+    put_resp = uta_enabled_restapi_client.put("/vrs_variation", json=allele_to_put)
+    assert put_resp.status_code == HTTPStatus.OK
+
+    # run test
+    resp = uta_enabled_restapi_client.get("/search_by_gene?gene=BRAF")
+    assert resp.status_code == HTTPStatus.OK
+    resp_json = resp.json()
+    assert resp_json["variations"]
