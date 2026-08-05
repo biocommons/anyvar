19f62b6db51a2ec5e346882e8cd5c627536b2d7e
Jennifer Bowser <76793787+jennifer-bowser@users.noreply.github.com>
2026-08-05 15:40:07 -0400

    remove boilerplate comments from 'alembic/env.py'
    Co-authored-by: James Stevenson <james.sharpsteen@gmail.com>

--- alembic/env.py
+++ alembic/env.py
@@ -13,27 +13,12 @@ from anyvar.storage.orm import Base
 
 dotenv.load_dotenv()
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
 config: Config = context.config
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
 target_metadata = Base.metadata
-
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
-
-# Don't override config if it's already been set
 if not config.get_main_option("sqlalchemy.url"):
     configure_alembic(config)
 
